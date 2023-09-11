0a53e89f85d7ff3d2ddc6c5768df342bf054633c
dg2003 <dg2003gh@gmail.com>
2023-09-11 14:32:28 -0300

    update togglePassword.js passwordInputList cons

--- assets/js/togglePassword.js
+++ assets/js/togglePassword.js
@@ -1,5 +1,21 @@
-const passwordTogger = document querySelectorAll("c-modal-toggle-password")
-const passawordinput = document querySelectorAll(
-    "c-modal_password-container in put"
-    );
+const passwordToggle = document.querySelectorAll(".c-modal__toggle-password");
+const passwordInputList = document.querySelectorAll(
+  ".c-modal__password-container input"
+);
 
+passwordToggle.forEach((element, index) => {
+  element.addEventListener("click", () => {
+    let passwordInput = passwordInputList[index];
+
+    if (
+      passwordInput.type === "password" &&
+      element.classList.contains("ri-eye-line")
+    ) {
+      passwordInput.type = "text";
+      element.classList.replace("ri-eye-line", "ri-eye-off-line");
+    } else {
+      passwordInput.type = "password";
+      element.classList.replace("ri-eye-off-line", "ri-eye-line");
+    }
+  });
+});
