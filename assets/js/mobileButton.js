3c2e99aa3f4d171f9f2a07a3733021fac2ef1bd1
dg2003 <dg2003gh@gmail.com>
2023-09-11 17:37:49 -0300

    add mobileButton.js | mobile menu logic

---
+++ assets/js/mobileButton.js
@@ -0,0 +1,15 @@
+if (window.innerWidth < 768) {
+  const mobileButton = document.getElementById("c-site-header__mobile-button");
+  const buttonsContainer = document.querySelector(
+    ".c-site-header__buttons-container "
+  );
+  mobileButton.addEventListener("click", () => {
+    buttonsContainer.style.transform = "translate(-50%, -50%) scale(1)";
+    document.body.classList.add("u-overflow-hidden");
+  });
+
+  buttonsContainer.addEventListener("click", () => {
+    buttonsContainer.style.transform = "scale(0)";
+    document.body.classList.remove("u-overflow-hidden");
+  });
+}
