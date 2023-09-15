e00ade3cc6e2340f2f5891ce605a3140cf4c2038
David Shelton <david.faria012@gmail.com>
2023-09-15 17:05:33 -0300

    Add new images in assets/imgs/stablishments > Update searchBar.js: > Clear code, rename clearCards() function -> resetSearchResult, Remove remCard() and ultCardForaVP() functions

--- assets/js/searchBar.js
+++ assets/js/searchBar.js
@@ -9,25 +9,36 @@ const searchContainer = document.querySelector("div.c-searching-container");
 // Resultado da pesquisa
 const searchResult = searchContainer.querySelector("div.c-searching-container__result");
 
+// Body
+const body = document.querySelector("body");
+
+// Array de filtro
+let filterArray = [];
+
+// Cards fictícios
 let arrayCard = [
   {
-    "image": "/assets/imgs/stablishments/default.png",
+    "image": "/assets/imgs/stablishments/happiness_coffee_shop.png",
     "title": "Happiness Coffee Shop",
     "description": "A comfortable place for you and your whole family. Our facilities have ramps, a room for guide dogs, menus in Pounds version..."
   },
   {
-    "image": "/assets/imgs/stablishments/default.png",
+    "image": "/assets/imgs/stablishments/zursky_tech.png",
     "title": "Zurski Tech",
     "description": "A comfortable place for you and your whole family. Our facilities have ramps, a room for guide dogs, menus in Pounds version..."
   },
   {
-    "image": "/assets/imgs/stablishments/default.png",
+    "image": "/assets/imgs/stablishments/green_vegan_restaurant.png",
     "title": "Green Vegan Restaurant",
+    "description": "A comfortable place for you and your whole family. Our facilities have ramps, a room for guide dogs, menus in Pounds version..."
+  },
+  {
+    "image": "/assets/imgs/stablishments/gold_hotel.png",
+    "title": "Gold Hotel",
     "description": "A comfortable place for you and your whole family. Our facilities have ramps, a room for guide dogs, menus in Pounds version..."
   }
 ];
 
-filterArray = [];
 
 searchBar.onkeyup = (e)=>{
   // Texto de pesquisa
@@ -41,6 +52,8 @@ searchBar.onkeyup = (e)=>{
         window.location.href = "./searchPage.html";
       }
 
+      clearSearchResult();
+
       // Filtrando cards
       filterArray = arrayCard.filter((card)=>{
         return card.title.toLowerCase().startsWith(searchText.toLowerCase());
@@ -53,96 +66,12 @@ searchBar.onkeyup = (e)=>{
   }
 };
 
-
-let scrollOld = window.scrollY
-
-body = document.querySelector("body")
-
 // Adiciona o evento clearCards em todas as tags de pesquisa
 // Na lista há uma função entries() que não sei o que é...
 tags = document.querySelectorAll("span.tag")
 for (let pos=0; pos < tags.length; pos++){
   tags[pos].addEventListener("click", clearCards)
 }
-
-// window.addEventListener("scroll", function() {
-
-//   scrollDirec = getScrollDirection()
-//   // Verifica se scroll está 
-//   // se movimentando para baixo ou para cima
-//   if (scrollDirec == "down") {
-//     if (ultCardDentroVP()) {
-//       addCard()
-//     }
-//   }
-//   else if (scrollDirec == "up") {
-//     if (ultCardForaVP()) {
-//       remCard()
-//     }
-//   }
-//   // Atualiza a última posição do scroll
-//   scrollOld = window.scrollY
-// })
-
-// Carrega os cards ao carregar a página
-function loadCards(){
-  for (i=0; i<3; i++){
-    addCard()
-  }
-}
-
-// Retorna a direção do scroll
-function getScrollDirection(){
-  // Verifica a direção do scroll
-  if (scrollOld > window.scrollY){
-    return "up"
-  }
-  else{
-    return "down" 
-  }
-}
-
-function ultCardDentroVP(){
-  /* Verifica se a base do último card está dentro da view port */
-
-  // Lista de cards
-  cards = searchContainer.querySelectorAll("article")
-  // Ultimo card
-  ultCard = cards[cards.length-1]
-
-  // Posição y da base do último card
-  ultCardPBOTT = ultCard.getBoundingClientRect().bottom
-  // Altura da viewport
-  alturaVP = window.innerHeight
-
-  // Verifica se posição da base do 
-  // último card é menor que a altura da viewport
-  if (ultCardPBOTT <= alturaVP)
-    return true
-  else
-    return false
-}
-
-function ultCardForaVP(){
-  /* Verifica se a base do último card está dentro da view port */
-
-  // Lista de cards
-  cards = searchContainer.querySelectorAll("article")
-  // Ultimo card
-  ultCard = cards[cards.length-1]
-  // Posição y da base do último card
-  ultCardPTOP = ultCard.getBoundingClientRect().top
-  // Altura da viewport
-  alturaVP = window.innerHeight
-
-  // Verifica se posição da base do 
-  // último card é menor que a altura da viewport
-  if (ultCardPTOP >= alturaVP * 0.85)
-    return true
-  else
-    return false
-}
-
 
 function addCard(index){
     // Get search container result
@@ -198,17 +127,11 @@ function addCard(index){
       </div>
     `
     filterArray[index].idvaga ++
-    searchContainer.appendChild(card)
+    searchResult.appendChild(card)
 }
 
-function remCard(){
-    // Get search container result
-    cardList = searchContainer.querySelectorAll("article")
-    // Remove the last card
-    searchContainer.removeChild(cardList[cardList.length -1])
-}
 
-function clearCards(){
+function resetSearchResult(){
   /* 
     Limpa os cards do container de pesquisa
   */
@@ -217,4 +140,12 @@ function clearCards(){
       <h1>No stablishments found</h1>
     </div>
   `
+}
+
+function clearSearchResult(){
+  /* 
+    Limpa os cards do container de pesquisa
+  */
+  searchResult.innerHTML = ""
+  
 }
