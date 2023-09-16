ad016d6f49945d455107abd2288b0e6dd3f8d60e
dg2003 <dg2003gh@gmail.com>
2023-09-16 16:14:54 -0300

    add muralcard initial logic commented

---
+++ assets/js/muralCards.js
@@ -0,0 +1,6 @@
+const muralGrid = document.querySelector("#js-mural_section");
+
+/* document.addEventListener("scroll", () => {
+const card = new NewsCard("oi", muralGrid);
+  card.showCard();
+}); */
