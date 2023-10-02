587ce698386e28a784bd30461fddc7a7c809b2b8
Douglas Guimarães <dg2003gh@gmail.com>
2023-10-02 18:00:25 -0300

    add input events js file

---
+++ assets/js/inputEvents.js
@@ -0,0 +1,38 @@
+document.addEventListener("keydown", ({ key }) => {
+  const modal = document.querySelector(".c-modal.u-visible");
+  const searchInput = document.getElementById("c-search_input");
+  const searchButton = document.getElementById("c-search_button");
+  const toolsDropdownButton = document.getElementById("c-tools-dropdown");
+
+  switch (key) {
+    case "Escape":
+      if (modal != null) {
+        modal.classList.remove("u-visible");
+      }
+      break;
+    case "b":
+      toggleToolsDropdown();
+      break;
+    case "Home":
+      window.location.href = "../html/index.html";
+      break;
+    case "p":
+      searchInput.focus();
+      break;
+    case "Enter":
+      if (document.activeElement == searchInput) {
+        searchButton.focus();
+        searchButton.click();
+      }
+      break;
+    case "Tab":
+      if (modal != null) {
+        modal.children.tabIndex = 5;
+      }
+      break;
+    case "Enter":
+      if (document.activeElement == toolsDropdownButton) {
+        toolsDropdownButton.click();
+      }
+  }
+});
