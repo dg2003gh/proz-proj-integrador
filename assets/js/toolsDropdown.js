81cfb49188ab023fe3190e8e14b38235237cc00a
Douglas Guimarães <dg2003gh@gmail.com>
2023-10-02 18:01:57 -0300

    add on fucus and on click func

--- assets/js/toolsDropdown.js
+++ assets/js/toolsDropdown.js
@@ -1,3 +1,8 @@
 const toolsDropDown = document.getElementById("c-tools-dropdown");
-const toolsDropDownMenu = document.getElementById("c-tools-dropdown__menu");
-toolsDropDown.onclick = () => toolsDropDownMenu.classList.toggle("visible");
+
+function toggleToolsDropdown() {
+  const toolsDropDownMenu = document.getElementById("c-tools-dropdown__menu");
+  toolsDropDownMenu.classList.toggle("visible");
+}
+toolsDropDown.onclick = toggleToolsDropdown;
+toolsDropDown.onfocus = toggleToolsDropdown;
