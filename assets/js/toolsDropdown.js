efd95b183f65afc59e37699de11bc75d1ddbe1c8
Douglas Guimarães <dg2003gh@gmail.com>
2023-10-02 14:02:33 -0300

    add tools dropdown logic

---
+++ assets/js/toolsDropdown.js
@@ -0,0 +1,3 @@
+const toolsDropDown = document.getElementById("c-tools-dropdown");
+const toolsDropDownMenu = document.getElementById("c-tools-dropdown__menu");
+toolsDropDown.onclick = () => toolsDropDownMenu.classList.toggle("visible");
