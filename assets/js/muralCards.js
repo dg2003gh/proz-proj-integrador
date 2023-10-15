a6eb7756bc6f6892121db3134daaae9c3058737c
Douglas Guimarães <dg2003gh@gmail.com>
2023-10-15 17:03:48 -0300

    add load more news card button

--- assets/js/muralCards.js
+++ assets/js/muralCards.js
@@ -1,9 +1,17 @@
+import NewsCard from "./classes/NewsCard";
+
 const muralGrid = document.querySelector("#js-mural_section");
+
 const newsKey = () => {
   return "e81b33b2828647d0887a4ea12e9ff25a";
 };
 
-document.addEventListener("scroll", () => {
-  const card = new NewsCard("accessibility", muralGrid, newsKey());
-  card.showCard();
-});
+const cardCount = 5;
+function displayMoreCards() {
+  for (let index = 0; cardCount > index; index++) {
+    const card = new NewsCard("accessibility", muralGrid, newsKey());
+    card.showCard();
+  }
+}
+
+displayMoreCards();
