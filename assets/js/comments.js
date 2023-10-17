7b2a77eab50037524801731cfffdd1c291c1c270
Douglas Guimarães <dg2003gh@gmail.com>
2023-10-16 21:42:40 -0300

    add comments logic

---
+++ assets/js/comments.js
@@ -0,0 +1,62 @@
+const commentsBd = [
+  {
+    userName: "Juliano",
+    userComment: "Ótimo lugar, perfeito! Mas não atende minha deficiência. ",
+    userRate: 4,
+  },
+  {
+    userName: "andré",
+    userComment: "Maravilha de cafeteria!",
+    userRate: 5,
+  },
+  {
+    userName: "Lucas",
+    userComment: "Não gostei :/",
+    userRate: 2,
+  },
+];
+
+commentsBd.forEach((user, index) => {
+  const commentsContainer = document.getElementById("js-comments-container");
+  commentsContainer.innerHTML += `
+    <article
+                class="u-row-container u-full-width c-card u-tertiary-bg-color u-border-radius u-padding u-gap u-vertical-center"
+            >
+                <span class="u-column-container u-center">
+                <img
+                    alt=""
+                    src="../imgs/icons/user.svg"
+                    width="150px"
+                    height="150px"
+                />
+                <label for="commentary_textarea_commentary_section">
+                    ${commentsBd[index].userName}
+                </label>
+                </span>
+                <span class="u-column-container u-center u-full-width">
+                <span
+                    readonly
+                    class="c-card__text-input u-full-width u-margin u-text-center"
+                >
+                    ${commentsBd[index].userComment}
+                </span>
+                <footer>
+                    <span class="u-row-container u-gap c-card__rate-container">
+                    <small>Rating:</small>
+                    ${setRate(index)}
+                    </span>
+                </footer>
+                </span>
+            </article>
+`;
+});
+
+function setRate(bdIndex) {
+  for (let index = 0; index < 5; ++index) {
+    let star = '<i class="ri-star-fill"></i>';
+    if (index < commentsBd[bdIndex].userRate) {
+      star = '<i class="ri-star-fill u-highlight-color"></i>';
+    }
+    return star;
+  }
+}
